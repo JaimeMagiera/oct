a9f3b9eceebe7d71525ea77f3573ba650247906f
jaimelm <jaimelm@umich.edu>
2020-10-21 10:49:39 -0400

    updated for latest release

--- configureOKDInstallerEnvironment.sh
+++ configureOKDInstallerEnvironment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 install_folder=$1
 
-okd_install_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-09-18-202631/openshift-install-linux-4.5.0-0.okd-2020-09-18-202631.tar.gz"
+okd_install_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-install-linux-4.5.0-0.okd-2020-10-15-235428.tar.gz"
 
-okd_client_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-09-18-202631/openshift-client-linux-4.5.0-0.okd-2020-09-18-202631.tar.gz"
+okd_client_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-client-linux-4.5.0-0.okd-2020-10-15-235428.tar.gz"
 
 fcos_image_url="https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200907.3.0/x86_64/fedora-coreos-32.20200907.3.0-vmware.x86_64.ova"
 
