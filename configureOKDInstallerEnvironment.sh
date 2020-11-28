41370f8c4891afdb73349f860ad8b9db634bdd7d
jaimelm <jaimelm@umich.edu>
2020-11-28 06:23:36 -0500

    updated for OKD 4.6

--- configureOKDInstallerEnvironment.sh
+++ configureOKDInstallerEnvironment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 install_folder=$1
 
-okd_install_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-install-linux-4.5.0-0.okd-2020-10-15-235428.tar.gz"
+okd_install_url="https://github.com/openshift/okd/releases/download/4.6.0-0.okd-2020-11-27-200126/openshift-install-linux-4.6.0-0.okd-2020-11-27-200126.tar.gz"
 
-okd_client_url="https://github.com/openshift/okd/releases/download/4.5.0-0.okd-2020-10-15-235428/openshift-client-linux-4.5.0-0.okd-2020-10-15-235428.tar.gz"
+okd_client_url="https://github.com/openshift/okd/releases/download/4.6.0-0.okd-2020-11-27-200126/openshift-client-linux-4.6.0-0.okd-2020-11-27-200126.tar.gz"
 
-fcos_image_url="https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20200907.3.0/x86_64/fedora-coreos-32.20200907.3.0-vmware.x86_64.ova"
+fcos_image_url="https://builds.coreos.fedoraproject.org/prod/streams/stable/builds/32.20201104.3.0/x86_64/fedora-coreos-32.20201104.3.0-vmware.x86_64.ova"
 
 mkdir $1/bin
 echo "Downloading the OKD installer..."
