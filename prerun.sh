30f692334ced421f8cd67ed978efe6e9b8c1e7ef
jaimelm <jaimelm@umich.edu>
2021-01-30 21:57:26 -0500

    use full paths to binaries

--- prerun.sh
+++ prerun.sh
@@ -13,7 +13,7 @@ sed -i "s/\"timeouts\":{}/\"timeouts\":{\"httpResponseHeaders\":50,\"httpTotal\"
 # Change timeouts for Worker
 sed -i "s/\"timeouts\":{}/\"timeouts\":{\"httpResponseHeaders\":50,\"httpTotal\":600}/g" worker.ign
 
-sudo cp bootstrap.ign /var/www/html/bootstrap.ign
-sudo chown apache:apache /var/www/html/bootstrap.ign
-sudo restorecon -Rv /var/www/html/
+sudo /usr/bin/cp bootstrap.ign /var/www/html/bootstrap.ign
+sudo /usr/bin/chown apache:apache /var/www/html/bootstrap.ign
+sudo /usr/bin/restorecon -Rv /var/www/html/
 
