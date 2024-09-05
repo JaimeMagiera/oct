3a5aadb5fe9d5c2d8bb292669ebee981c790778f
Jaime Magiera <jaimelm@umich.edu>
2024-09-05 18:58:01 -0400

    moving things around and some edits

---
+++ terraform/modules/oct-coreos/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  description   = "The ami"
+  type          = string
+}
+
+variable "instance_type" {
+  description   = "The instance type"
+  type          = string
+}
+
+variable "tags_name" {
+  description 	= "The value of the name tag of the instance"
+  type 		= string
+}
+
+
